2b536bbb4e2a10161a8bd7fd234309946f2a5081
shubh-xmate <shuklashubham2277@gmail.com>
2024-07-18 16:37:17 +0530

    payroll api integrated

--- src/app/components/dashboard/main-content/payroll-details/payroll-details.component.ts
+++ src/app/components/dashboard/main-content/payroll-details/payroll-details.component.ts
@@ -3,24 +3,34 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Observer } from 'rxjs';
 import { PayrollDetailsService } from '../../../../services/payroll-details.service';
+import { IPayrollDetails } from './payrollDetails.model';
 
 @Component({
   selector: 'app-payroll-details',
   templateUrl: './payroll-details.component.html',
   styleUrls: ['./payroll-details.component.css'],
   standalone: true,
-  imports: [FormsModule, CommonModule]
+  imports: [CommonModule, FormsModule] // Adjust imports if necessary
 })
 export class PayrollDetailsComponent {
+  employeeId: number | null=null;
   payrollMonth: string = '';
-  payrollYear: number | null = null;
-  payrollDetails: any;
+  payrollYear:number | null = null;
+  payrollDetails: IPayrollDetails ={
+    employeeId: 0,
+    payrollMonth: '',
+    payrollYear:0,
+    deductions:0,
+    netSalary: 0
+  };
+  // payrollDetails: any;
   showDetails: boolean = false;
 
   constructor(private payrollService: PayrollDetailsService) {}
 
   onSubmit() {
-    if (this.payrollMonth && this.payrollYear) {
+
+    if (this.employeeId && this.payrollMonth && this.payrollYear) {
       const observer: Observer<any> = {
         next: (data) => {
           this.payrollDetails = data;
@@ -33,33 +43,7 @@ export class PayrollDetailsComponent {
         }
       };
 
-      this.payrollService.getPayrollDetails(this.payrollMonth, this.payrollYear).subscribe(observer);
+      this.payrollService.getPayrollDetails(this.employeeId, this.payrollMonth, this.payrollYear).subscribe(observer);
     }
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import { Component } from '@angular/core';
-//
-// @Component({
-//   selector: 'app-payroll-details',
-//   standalone: true,
-//   imports: [],
-//   templateUrl: './payroll-details.component.html',
-//   styleUrl: './payroll-details.component.css'
-// })
-// export class PayrollDetailsComponent {
-//
-// }
